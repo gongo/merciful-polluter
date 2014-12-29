7eaaeca986c17384b600a114cf0107b300ada7d7
Wataru MIYAGUNI <gonngo@gmail.com>
2014-12-29 16:39:59 +0900

    Add test for f3698c59bf2ce1fd8a126170e4abe312973c23f0

---
+++ test/BaseTest.php
@@ -0,0 +1,58 @@
+<?php
+namespace Gongo\MercifulPolluter\Test;
+
+use \PHPUnit_Framework_TestCase;
+use Gongo\MercifulPolluter\Base;
+
+class Polluter extends Base
+{
+    public function check($theKey)
+    {
+        return $this->ignoringVariable($theKey);
+    }
+}
+
+class BaseTest extends PHPUnit_Framework_TestCase
+{
+    private $object = null;
+    
+    protected function setUp()
+    {
+        $this->object = new Polluter;
+    }
+
+    public function testIgnoringVariable()
+    {
+        $ignores = array(
+            'GLOBALS',
+            '_SERVER',
+            '_GET',
+            '_POST',
+            '_FILES',
+            '_REQUEST',
+            '_SESSION',
+            '_ENV',
+            '_COOKIE',
+            'php_errormsg',
+            'HTTP_RAW_POST_DATA',
+            'http_response_header',
+            'argc',
+            'argv',
+        );
+
+        foreach ($ignores as $key) {
+            $this->assertTrue($this->object->check($key));
+        }
+
+        $throughs = array(
+            '__FILE__',
+            'foo',
+            'bar',
+            '_GETTT'
+        );
+
+        foreach ($throughs as $key) {
+            $this->assertFalse($this->object->check($key));
+        }
+    }
+}
